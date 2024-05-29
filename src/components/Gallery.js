09fb2cfb97338eec4c3500f81cb1737f7248421d
Elijah Oquendo <epoquendo2@gmail.com>
2024-05-29 19:06:31 -0400

    Project Update

---
+++ src/components/Gallery.js
@@ -0,0 +1,21 @@
+import React from "react";
+import GalleryItem from './GalleryItem';
+
+
+function Gallery(props) {
+
+
+const songItems = props.songData.map((item, index) => {
+    return(
+        <GalleryItem  key={index} item={item} />
+    )
+})
+
+    return (
+        <div>
+          {songItems}
+        </div>
+    )
+}
+
+export default Gallery;
